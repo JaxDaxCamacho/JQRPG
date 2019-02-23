e30db4c710ddec3f44e4dc31a09c6674271545c6
joao david Camacho <Joaodavidosky@gmail.com>
2019-02-23 13:33:25 +0000

    base

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="pt-pt">
+	<head>
+
+		<meta charset="UTF-8">
+		<meta name="description" content="Meta tags">
+		<meta name="keywords" content="acin,bootcamp,ECMAscript">
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+		<meta name="author" content="David Camacho">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles/style.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+		<title>Dungeon Game</title>
+	</head>
+
+<body>
+<canvas  id="myCanvas" width="800" height="600" ></canvas>
+</body>
+
+<script src="game.js"></script>
+<script src="//cdn.jsdelivr.net/npm/phaser@3.16.2/dist/phaser.js"></script>
+<script src="//cdn.jsdelivr.net/npm/phaser@3.16.2/dist/phaser.min.js"></script> 
+</html>
